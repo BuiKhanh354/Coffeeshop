a4a52aca0e06f35408ef185a8b999c79affadbec
Bui Viet Khanh <2254810078@vaa.edu.vn>
2026-01-15 22:20:07 +0700

    Initial CoffeeShop project

---
+++ src/CoffeeShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CoffeeShop.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
